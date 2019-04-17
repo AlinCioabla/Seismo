660f3622517256a414e6142314e8d8577deb6cb6
Alin Craciun <craciun96alin@gmail.com>
2019-04-17 21:15:30 +0300

    Desktop app

---
+++ seismograf/Seismograf/seismograf/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace seismograf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.angularGauge1 = new LiveCharts.WinForms.AngularGauge();
+            this.angularGauge2 = new LiveCharts.WinForms.AngularGauge();
+            this.angularGauge3 = new LiveCharts.WinForms.AngularGauge();
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Axis X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(367, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Axis Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(661, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Axis Z";
+            // 
+            // angularGauge1
+            // 
+            this.angularGauge1.Location = new System.Drawing.Point(12, 12);
+            this.angularGauge1.Name = "angularGauge1";
+            this.angularGauge1.Size = new System.Drawing.Size(207, 109);
+            this.angularGauge1.TabIndex = 3;
+            this.angularGauge1.Text = "angularGauge1";
+            // 
+            // angularGauge2
+            // 
+            this.angularGauge2.Location = new System.Drawing.Point(286, 12);
+            this.angularGauge2.Name = "angularGauge2";
+            this.angularGauge2.Size = new System.Drawing.Size(207, 109);
+            this.angularGauge2.TabIndex = 4;
+            this.angularGauge2.Text = "angularGauge2";
+            // 
+            // angularGauge3
+            // 
+            this.angularGauge3.Location = new System.Drawing.Point(581, 12);
+            this.angularGauge3.Name = "angularGauge3";
+            this.angularGauge3.Size = new System.Drawing.Size(207, 109);
+            this.angularGauge3.TabIndex = 5;
+            this.angularGauge3.Text = "angularGauge3";
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Location = new System.Drawing.Point(12, 158);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(776, 234);
+            this.cartesianChart1.TabIndex = 6;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 406);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Waiting for a client.";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(684, 390);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 33);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Start server";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cartesianChart1);
+            this.Controls.Add(this.angularGauge3);
+            this.Controls.Add(this.angularGauge2);
+            this.Controls.Add(this.angularGauge1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private LiveCharts.WinForms.AngularGauge angularGauge1;
+        private LiveCharts.WinForms.AngularGauge angularGauge2;
+        private LiveCharts.WinForms.AngularGauge angularGauge3;
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ seismograf/Seismograf/seismograf/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Net;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Net.Sockets;
+
+namespace seismograf
+{
+    public partial class Form1 : Form
+    {
+        NetworkStream stream;
+        TcpClient client;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label4.Text = "Waiting for a client.";
+            TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 80);
+
+            server.Start();
+            label4.Text = "Server has started on 127.0.0.1:80.{0}Waiting for a connection...";
+
+            client = server.AcceptTcpClient();
+
+            label4.Text = "A client connected.";
+
+            stream = client.GetStream();
+
+            //enter to an infinite cycle to be able to handle every change in stream
+
+            while (client.Available < 3)
+            {
+                // wait for enough bytes to be available
+            }
+
+            Byte[] bytes = new Byte[client.Available];
+
+            stream.Read(bytes, 0, bytes.Length);
+
+            //translate bytes of request to string
+            String data = Encoding.UTF8.GetString(bytes);
+
+            if (Regex.IsMatch(data, "^GET"))
+            {
+
+            }
+            else
+            {
+
+            }
+
+            if (new System.Text.RegularExpressions.Regex("^GET").IsMatch(data))
+            {
+                const string eol = "\r\n"; // HTTP/1.1 defines the sequence CR LF as the end-of-line marker
+
+                Byte[] response = Encoding.UTF8.GetBytes("HTTP/1.1 101 Switching Protocols" + eol
+                    + "Connection: Upgrade" + eol
+                    + "Upgrade: websocket" + eol
+                    + "Sec-WebSocket-Accept: " + Convert.ToBase64String(
+                        System.Security.Cryptography.SHA1.Create().ComputeHash(
+                            Encoding.UTF8.GetBytes(
+                                new System.Text.RegularExpressions.Regex("Sec-WebSocket-Key: (.*)").Match(data).Groups[1].Value.Trim() + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+                            )
+                        )
+                    ) + eol
+                    + eol);
+
+                stream.Write(response, 0, response.Length);
+            }
+            button1.Visible = false;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (stream != null && stream.DataAvailable)
+            {
+                Byte[] bytes1 = new Byte[client.Available];
+                stream.Read(bytes1, 0, bytes1.Length);
+                var b = GetDecodedData(bytes1, bytes1.Length);
+                label4.Text = b;
+            }
+        }
+        public static string GetDecodedData(byte[] buffer, int length)
+        {
+            byte b = buffer[1];
+            int dataLength = 0;
+            int totalLength = 0;
+            int keyIndex = 0;
+
+            if (b - 128 <= 125)
+            {
+                dataLength = b - 128;
+                keyIndex = 2;
+                totalLength = dataLength + 6;
+            }
+
+            if (b - 128 == 126)
+            {
+                dataLength = BitConverter.ToInt16(new byte[] { buffer[3], buffer[2] }, 0);
+                keyIndex = 4;
+                totalLength = dataLength + 8;
+            }
+
+            if (b - 128 == 127)
+            {
+                dataLength = (int)BitConverter.ToInt64(new byte[] { buffer[9], buffer[8], buffer[7], buffer[6], buffer[5], buffer[4], buffer[3], buffer[2] }, 0);
+                keyIndex = 10;
+                totalLength = dataLength + 14;
+            }
+
+            if (totalLength > length)
+                throw new Exception("The buffer length is small than the data length");
+
+            byte[] key = new byte[] { buffer[keyIndex], buffer[keyIndex + 1], buffer[keyIndex + 2], buffer[keyIndex + 3] };
+
+            int dataIndex = keyIndex + 4;
+            int count = 0;
+            for (int i = dataIndex; i < totalLength; i++)
+            {
+                buffer[i] = (byte)(buffer[i] ^ key[count % 4]);
+                count++;
+            }
+
+            return Encoding.ASCII.GetString(buffer, dataIndex, dataLength);
+        }
+    }
+}
